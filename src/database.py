4700c1ddff7b7505f268241a99865bf83eac63ae
Ljupcho Stefanov <ljupchos@iborn.net>
2020-05-20 10:12:22 +0200

    adding files and docker files and cleaning up the environment

--- src/database.py
+++ src/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://localhost:3306/server_helper"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:Stefanov+1998@localhost:3306/referral"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
--- src/models.py
+++ src/models.py
@@ -3,44 +3,45 @@ from sqlalchemy.orm import relationship
 
 from .database import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    refferal_link = Column(String, unique=True, index=True)
+    referral_link = Column(String(50), unique=True, index=True)
     credits = Column(Float, default=0.0)
     administrator = Column(Boolean, default=False)
-    refferaled_id = Column(Integer, ForeignKey('users.id'))
+    referraled_id = Column(Integer, nullable=True)
 
 
 class Plan(Base):
     __tablename__ = "plans"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    num_servers = Column(Integer)
-    price = Column(Float)
+    name = Column(String(50), nullable=False)
+    num_servers = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
 
 
-class Vaucher(Base):
-    __tablename__ = "vaucher"
+class Voucher(Base):
+    __tablename__ = "voucher"
 
     id = Column(Integer, primary_key=True, index=True)
-    code = Column(String, unique=True)
+    code = Column(String(50), unique=True)
     is_used = Column(Boolean, default=False)
-    amount = Column(Float)
-    start_date = Column(DateTime)
-    end_date = Column(DateTime)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    amount = Column(Float, nullable=False)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True)
 
 
 class PromoCode(Base):
     __tablename__ = "promo_code"
 
     id = Column(Integer, primary_key=True, index=True)
-    code = Column(String, unique=True)
+    code = Column(String(50), unique=True)
     is_used = Column(Boolean, default=False)
     discount_percentage = Column(Float)
     start_date = Column(DateTime)
     end_date = Column(DateTime)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True)
